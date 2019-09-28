07846483e52b273ea49a37673e86ef255d04c227
Willian Chu <willianchu@hotmail.com>
2019-09-27 23:20:21 -0400

    Add project files.

---
+++ HelloWorldForm.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Hello_World
+{
+    partial class HelloWorldForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.messageLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.messageLabel.AutoEllipsis = true;
+            this.messageLabel.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.messageLabel.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.messageLabel.Location = new System.Drawing.Point(311, 103);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(166, 42);
+            this.messageLabel.TabIndex = 0;
+            this.messageLabel.Text = "message";
+            this.messageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // HelloWorldForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 371);
+            this.Controls.Add(this.messageLabel);
+            this.Name = "HelloWorldForm";
+            this.Text = "Hello World Form";
+            this.Load += new System.EventHandler(this.HelloWorldForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label messageLabel;
+    }
+}
+
